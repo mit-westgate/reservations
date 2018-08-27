a3b816c07ff2bb4af5c27b5cede0c673fdcb33ba
Yasushi Sakai <yasushis@mit.edu>
2018-08-27 14:59:51 -0400

    admin.py

--- admin.py
+++ admin.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python
 
-print("Content-type: text/html\n\n")
-print("admin")
+import sys
+import os
 
+main_dir = os.path.dirname(os.path.abspath(__file__))
+lib_dir = os.path.join(main_dir, 'lib')
+
+# Append all packages in ./lib to path
+for pkg in os.listdir(lib_dir):
+    sys.path.insert(0, os.path.join(lib_dir, pkg))
+
+# TODO: cherry pick what I need...
+
+from jinja2 import Environment, PackageLoader, select_autoescape
+env = Environemnt(
+        loader = PackageLoader('resevations', 'templates'),
+        autoescape = select_autoescape(['html', 'xml'])
+)
+
+
+template = env.get_template("admin.html")
+print template.render()
