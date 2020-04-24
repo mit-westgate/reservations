64771cbc45f48eb7d2b2b7f9fe4b866e48a63302
Yasushi Sakai <yasushis@mit.edu>
2020-04-24 12:08:22 -0400

    added new officers

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 admin_email = "westgate-ipc@mit.edu"
-super_users = ["yasushis", "brij","nili", "naomic"]
+super_users = ["yasushis", "brij", "nili", "naomic", "cyoo", "dipinoch"]
 database_file = "reservation_db.sqlite"
